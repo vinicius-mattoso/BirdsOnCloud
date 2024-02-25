c01d3ca02e4bff276edde3e6b49ea1f698ff5b17
Vinicius Mattoso <vinicius.vmrs@gmail.com>
2024-02-25 19:53:53 -0300

    [add] the sum.py with the sum code

---
+++ utils/sum.py
@@ -0,0 +1,15 @@
+# Function to make the sum of two terms
+
+def sum(number1, number2):
+    '''
+    This function is used to make the sum of two terms.
+
+    num1 -> float number
+    num2 -> float number
+
+    return num1 + num2
+    '''
+
+    result = number1 + number2
+
+    return result
